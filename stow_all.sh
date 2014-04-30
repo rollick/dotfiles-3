2a29bc65f7208669ccb3cacc890b9301f7f49c13
Raphael Scholer <rscholer@gmx.de>
2014-04-30 23:52:50 +0200

    Add moc configuration

--- stow_all.sh
+++ stow_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 stow --verbose -R -t ${HOME} appearance
+stow --verbose -R -t ${HOME} moc
 stow --verbose -R -t ${HOME} openbox
 stow --verbose -R -t ${HOME} x.org
 stow --verbose -R -t ${HOME} zsh-config

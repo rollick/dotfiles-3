9483d78cdcb1eb3479e10c63c0b82dd4322e61ac
Raphael Scholer <rscholer@gmx.de>
2014-05-01 16:19:42 +0200

    Add tmux configuration

--- stow_all.sh
+++ stow_all.sh
@@ -7,5 +7,6 @@ stow --verbose -R -t ${HOME} appearance
 stow --verbose -R -t ${HOME} moc
 stow --verbose -R -t ${HOME} openbox
 stow --verbose -R -t ${HOME} ssh
+stow --verbose -R -t ${HOME} tmux
 stow --verbose -R -t ${HOME} x.org
 stow --verbose -R -t ${HOME} zsh-config

3741655d9c557647a3c874cda19fafc012ef9e7c
Raphael Scholer <rscholer@gmx.de>
2014-05-01 15:31:47 +0200

    stow_all.sh: Make sure some directories are not created as a link

--- stow_all.sh
+++ stow_all.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mkdir -p "${XDG_CONFIG_HOME:-${HOME}/.config}"
+mkdir -p "${XDG_DATA_HOME:-${HOME}/.local/share}"
+
 stow --verbose -R -t ${HOME} appearance
 stow --verbose -R -t ${HOME} moc
 stow --verbose -R -t ${HOME} openbox

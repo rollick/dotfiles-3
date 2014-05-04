2200051d59f051d0ecf0e7ccf4924b5ba1180ace
Raphael Scholer <rscholer@gmx.de>
2014-05-05 00:07:20 +0200

    urxvt: Small fix

--- stow_all.sh
+++ stow_all.sh
@@ -4,6 +4,7 @@ mkdir -p "${HOME}/.ssh"
 mkdir --verbose -p "${HOME}/.unison"
 mkdir -p "${XDG_CONFIG_HOME:-${HOME}/.config}"
 mkdir -p "${XDG_DATA_HOME:-${HOME}/.local/share}"
+mkdir -p "${XDG_DATA_HOME:-${HOME}/.local/share/urxvt}"
 
 stow --verbose -R -t ${HOME} appearance
 stow --verbose -R -t ${HOME} moc

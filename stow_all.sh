e0244d40245ad259a23f6c907f9263d66c1b8e42
Raphael Scholer <rscholer@gmx.de>
2014-05-26 00:45:40 +0200

    Replace gnome-terminal with terminator

--- stow_all.sh
+++ stow_all.sh
@@ -13,6 +13,7 @@ stow --verbose -R -t ${HOME} moc
 stow --verbose -R -t ${HOME} openbox
 stow --verbose -R -t ${HOME} shells
 stow --verbose -R -t ${HOME} ssh
+stow --verbose -R -t ${HOME} terminator
 stow --verbose -R -t ${HOME} tmux
 stow --verbose -R -t ${HOME} unison
 stow --verbose -R -t ${HOME} virtualenvs

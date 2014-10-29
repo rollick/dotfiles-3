629f114a72cab38340fe277d00922b5988a80f56
Raphael Scholer <rscholer@gmx.de>
2014-10-29 02:47:03 +0100

    Some improvements for stow_all.sh

--- stow_all.sh
+++ stow_all.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
-mkdir --verbose -p "${HOME}/.moc"
 mkdir --verbose -p "${HOME}/.ssh"
-mkdir --verbose -p "${HOME}/.unison"
-mkdir --verbose -p "${HOME}/.icons"
-mkdir --verbose -p "${HOME}/.themes"
 mkdir --verbose -p "${HOME}/.vim"
-mkdir --verbose -p "${CONFIG_HOME}"
-mkdir --verbose -p "${CONFIG_HOME}/gtk-3.0"
-mkdir --verbose -p "${CONFIG_HOME}/xfce4/xfconf/xfce-perchannel-xml"
-
-stow --verbose -R -t ${HOME} appearance
-stow --verbose -R -t ${HOME} cups
-stow --verbose -R -t ${HOME} moc
-stow --verbose -R -t ${HOME} mpv
-stow --verbose -R -t ${HOME} octave
-stow --verbose -R -t ${HOME} openbox
-stow --verbose -R -t ${HOME} profile-cleaner
 stow --verbose -R -t ${HOME} shells
-stow --verbose -R -t ${HOME} smallterm
 stow --verbose -R -t ${HOME} ssh
 stow --verbose -R -t ${HOME} tmux
-stow --verbose -R -t ${HOME} unison
 stow --verbose -R -t ${HOME} vim
-stow --verbose -R -t ${HOME} x.org
 stow --verbose -R -t ${HOME} zsh
+
+if [[ ${UID} -ne 0 ]]; then
+	CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+	mkdir --verbose -p "${HOME}/.moc"
+	mkdir --verbose -p "${HOME}/.unison"
+	mkdir --verbose -p "${HOME}/.icons"
+	mkdir --verbose -p "${HOME}/.themes"
+	mkdir --verbose -p "${CONFIG_HOME}"
+	mkdir --verbose -p "${CONFIG_HOME}/gtk-3.0"
+	mkdir --verbose -p "${CONFIG_HOME}/xfce4/xfconf/xfce-perchannel-xml"
+	stow --verbose -R -t ${HOME} appearance
+	stow --verbose -R -t ${HOME} cups
+	stow --verbose -R -t ${HOME} moc
+	stow --verbose -R -t ${HOME} mpv
+	stow --verbose -R -t ${HOME} octave
+	stow --verbose -R -t ${HOME} openbox
+	stow --verbose -R -t ${HOME} profile-cleaner
+	stow --verbose -R -t ${HOME} smallterm
+	stow --verbose -R -t ${HOME} unison
+	stow --verbose -R -t ${HOME} x.org
+fi

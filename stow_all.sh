79ae4293d85379e1490ef68ab814dfa15c726db4
Raphael Scholer <rscholer@gmx.de>
2015-02-01 09:34:55 +0100

    Replace sakura with urxvt

--- stow_all.sh
+++ stow_all.sh
@@ -19,7 +19,6 @@ if [[ ${UID} -ne 0 ]]; then
 	stow --verbose -R -t ${HOME} octave
 	stow --verbose -R -t ${HOME} openbox
 	stow --verbose -R -t ${HOME} profile-cleaner
-	stow --verbose -R -t ${HOME} sakura
 	stow --verbose -R -t ${HOME} unison
 	stow --verbose -R -t ${HOME} x.org
 fi

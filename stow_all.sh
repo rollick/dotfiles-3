0c3678260efb97225875d7974f2942310df76ab0
Raphael Scholer <rscholer@gmx.de>
2015-03-13 22:17:47 +0100

    Add devilspie configuration

--- stow_all.sh
+++ stow_all.sh
@@ -17,6 +17,7 @@ if [[ ${UID} -ne 0 ]]; then
 	mkdir --verbose -p "${CONFIG_HOME}/xfce4/xfconf/xfce-perchannel-xml"
 	stow --verbose -R -t ${HOME} appearance
 	stow --verbose -R -t ${HOME} cups
+	stow --verbose -R -t ${HOME} devilspie2
 	stow --verbose -R -t ${HOME} fontconfig
 	stow --verbose -R -t ${HOME} octave
 	stow --verbose -R -t ${HOME} openbox

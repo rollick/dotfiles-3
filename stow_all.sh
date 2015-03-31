faa604b2cafcc4a166fc40a42ace4385f2c130c2
Raphael Scholer <rascholer@gmail.com>
2015-03-31 21:54:19 +0200

    Switch to Xfce4 permanently

--- stow_all.sh
+++ stow_all.sh
@@ -11,14 +11,11 @@ stow --verbose -R -t ${HOME} zsh
 if [[ ${UID} -ne 0 ]]; then
 	CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 	mkdir --verbose -p "${HOME}/.unison"
-	mkdir --verbose -p "${HOME}/.themes"
 	mkdir --verbose -p "${CONFIG_HOME}"
 	mkdir --verbose -p "${CONFIG_HOME}/gtk-3.0"
-	stow --verbose -R -t ${HOME} DE
 	stow --verbose -R -t ${HOME} appearance
 	stow --verbose -R -t ${HOME} fontconfig
 	stow --verbose -R -t ${HOME} octave
-	stow --verbose -R -t ${HOME} openbox
 	stow --verbose -R -t ${HOME} pacman
 	stow --verbose -R -t ${HOME} profile-cleaner
 	stow --verbose -R -t ${HOME} simpleterm

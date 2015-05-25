e6104b00f953a5d11e37992fca469be8ed5192db
Raphael Scholer <rascholer@gmail.com>
2015-05-25 04:04:02 +0200

    python: Print info about startup tweaks

--- python/.config/python_startup.py
+++ python/.config/python_startup.py
@@ -1,2 +1,9 @@
-from imp import reload
-from pprint import pprint
+#!/bin/env python
+from imp import reload  # noqa
+from pprint import pprint  # noqa
+
+print('\n########################################')
+print('# Startup tweaks:')
+print('#   - Import imp.reload() as reload()')
+print('#   - Import pprint.pprint() as pprint()')
+print('########################################\n')

bd621d93b7c267a3537c0083c9b63f07dfe71aa1
Raphael Scholer <rascholer@gmail.com>
2015-06-13 22:54:55 +0200

    Ensure correct appearance for programs run as root

--- stow_all.sh
+++ stow_all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
 mkdir --verbose -p "${HOME}/.ssh"
+mkdir --verbose -p "${HOME}/.themes"
 mkdir --verbose -p "${HOME}/.vim"
+mkdir --verbose -p "${CONFIG_HOME}/gtk-3.0"
+stow --verbose -R -t ${HOME} appearance
+stow --verbose -R -t ${HOME} fontconfig
 stow --verbose -R -t ${HOME} git
 stow --verbose -R -t ${HOME} python
 stow --verbose -R -t ${HOME} shells
@@ -10,18 +16,11 @@ stow --verbose -R -t ${HOME} vim
 stow --verbose -R -t ${HOME} zsh
 
 if [[ ${UID} -ne 0 ]]; then
-	CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
-	DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
 	mkdir --verbose -p "${HOME}/.moc"
 	mkdir --verbose -p "${HOME}/.unison"
-	mkdir --verbose -p "${HOME}/.themes"
-	mkdir --verbose -p "${CONFIG_HOME}"
 	mkdir --verbose -p "${CONFIG_HOME}/autostart"
-	mkdir --verbose -p "${CONFIG_HOME}/gtk-3.0"
 	mkdir --verbose -p "${DATA_HOME}/applications"
 	stow --verbose -R -t ${HOME} DE
-	stow --verbose -R -t ${HOME} appearance
-	stow --verbose -R -t ${HOME} fontconfig
 	stow --verbose -R -t ${HOME} moc
 	stow --verbose -R -t ${HOME} openbox
 	stow --verbose -R -t ${HOME} pacman

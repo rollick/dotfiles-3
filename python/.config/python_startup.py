e7d84c8eb0028ee0fec283c2bd9f195b34c6f90f
Raphael Scholer <rascholer@gmail.com>
2015-09-29 05:40:23 +0200

    python - Overwrite default history path

--- python/.config/python_startup.py
+++ python/.config/python_startup.py
@@ -1,10 +1,33 @@
 #!/bin/env python
 """This file will be executed when running python as an shell."""
+import atexit
 from imp import reload  # noqa
+import os
 from pprint import pprint  # noqa
+import readline
+import sys
 
-print('\n########################################')
-print('# Startup tweaks:')
-print('#   - Import imp.reload() as reload()')
-print('#   - Import pprint.pprint() as pprint()')
-print('########################################\n')
+HISTFILE = os.path.join(
+    os.getenv('XDG_CACHE_HOME') or '~/.cache',
+    ''.join(
+        (
+            'python',
+            str(sys.version_info.major),
+            '_history'
+        )
+    )
+)
+
+try:
+    readline.read_history_file(HISTFILE)
+except IOError:
+    pass
+
+atexit.register(readline.write_history_file, HISTFILE)
+
+# Cleanup
+del HISTFILE
+del atexit
+del os
+del readline
+del sys

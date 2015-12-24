bf81587756ebe4fbf161fc625cee87fc4860b026
Raphael Scholer <rascholer@gmail.com>
2015-12-24 08:45:17 +0100

    python - Remove redirection of history files

--- python/.config/python_startup.py
+++ python/.config/python_startup.py
@@ -1,33 +1,8 @@
 #!/bin/env python
 """This file will be executed when running python as an shell."""
-import atexit
 from imp import reload  # noqa
-import os
 from pprint import pprint  # noqa
-import readline
 import sys
 
-HISTFILE = os.path.join(
-    os.getenv('XDG_CACHE_HOME', '~/.cache'),
-    ''.join(
-        (
-            'python',
-            str(sys.version_info.major),
-            '_history'
-        )
-    )
-)
-
-try:
-    readline.read_history_file(HISTFILE)
-except IOError:
-    pass
-
-atexit.register(readline.write_history_file, os.path.expanduser(HISTFILE))
-
 # Cleanup
-del HISTFILE
-del atexit
-del os
-del readline
 del sys

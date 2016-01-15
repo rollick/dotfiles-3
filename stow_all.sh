48c780664a305bf040eb1814fa2c3ce31a1764d0
Raphael Scholer <rascholer@gmail.com>
2016-01-15 17:39:57 +0100

    root doesn't need X11 configuration

--- stow_all.sh
+++ stow_all.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-mkdir --verbose -p "${HOME}/.config/gtk-2.0"
-mkdir --verbose -p "${HOME}/.config/gtk-3.0"
 mkdir --verbose -p "${HOME}/.config/ranger"
 mkdir --verbose -p "${HOME}/.config/gnupg"
 mkdir --verbose -p "${HOME}/.ssh"
 chmod --verbose 700 "${HOME}/.config/gnupg"
 chmod --verbose 700 "${HOME}/.ssh"
-stow --verbose -R -t ${HOME} appearance
-stow --verbose -R -t ${HOME} fontconfig
 stow --verbose -R -t ${HOME} git
 stow --verbose -R -t ${HOME} gnupg
 stow --verbose -R -t ${HOME} neovim
@@ -16,19 +12,24 @@ stow --verbose -R -t ${HOME} ranger
 stow --verbose -R -t ${HOME} shells
 stow --verbose -R -t ${HOME} ssh
 stow --verbose -R -t ${HOME} tmux
-stow --verbose -R -t ${HOME} x.org
+stow --verbose -R -t ${HOME} xdg
 stow --verbose -R -t ${HOME} zsh
 
 if [[ ${UID} -ne 0 ]]; then
+	mkdir --verbose -p "${HOME}/.config/gtk-2.0"
+	mkdir --verbose -p "${HOME}/.config/gtk-3.0"
 	mkdir --verbose -p "${HOME}/.local/bin"
 	mkdir --verbose -p "${HOME}/.unison"
+	stow --verbose -R -t ${HOME} appearance
 	stow --verbose -R -t ${HOME} dunst
+	stow --verbose -R -t ${HOME} fontconfig
 	stow --verbose -R -t ${HOME} i3
 	stow --verbose -R -t ${HOME} pacman
 	stow --verbose -R -t ${HOME} pacaur
 	stow --verbose -R -t ${HOME} profile-cleaner
 	stow --verbose -R -t ${HOME} scripts
 	stow --verbose -R -t ${HOME} unison
+	stow --verbose -R -t ${HOME} x.org
 elif [[ ${UID} -eq 0 ]]; then
 	stow --verbose -R -t ${HOME} su
 fi

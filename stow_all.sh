cd8235066a2bea365567da30beb702282742e9a1
Raphael Scholer <rascholer@gmail.com>
2016-02-09 01:23:19 +0100

    Remove general scripts

--- stow_all.sh
+++ stow_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir --verbose -p "${HOME}/.config/ranger"
 mkdir --verbose -p "${HOME}/.config/gnupg"
+mkdir --verbose -p "${HOME}/.local/bin"
 mkdir --verbose -p "${HOME}/.ssh"
-chmod --verbose 700 "${HOME}/.config/gnupg"
 chmod --verbose 700 "${HOME}/.ssh"
 stow --verbose -R -t ${HOME} flake8
 stow --verbose -R -t ${HOME} git
@@ -19,7 +19,6 @@ if [[ ${UID} -ne 0 ]]; then
 	mkdir --verbose -p "${HOME}/.config/cmus"
 	mkdir --verbose -p "${HOME}/.config/gtk-2.0"
 	mkdir --verbose -p "${HOME}/.config/gtk-3.0"
-	mkdir --verbose -p "${HOME}/.local/bin"
 	mkdir --verbose -p "${HOME}/.unison"
 	stow --verbose -R -t ${HOME} appearance
 	stow --verbose -R -t ${HOME} cmus
@@ -29,7 +28,6 @@ if [[ ${UID} -ne 0 ]]; then
 	stow --verbose -R -t ${HOME} pacman
 	stow --verbose -R -t ${HOME} pacaur
 	stow --verbose -R -t ${HOME} profile-cleaner
-	stow --verbose -R -t ${HOME} scripts
 	stow --verbose -R -t ${HOME} unison
 	stow --verbose -R -t ${HOME} x.org
 fi

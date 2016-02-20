d571a60d72e1e49980e21984783e0972faa9bc92
Raphael Scholer <rascholer@gmail.com>
2016-02-20 23:05:56 +0100

    stow-all.sh - Ignore .venv

--- stow_all.sh
+++ stow_all.sh
@@ -3,17 +3,17 @@ mkdir --verbose -p "${HOME}/.config/gnupg"
 mkdir --verbose -p "${HOME}/.local/share/virtualenvs"
 mkdir --verbose -p "${HOME}/.ssh"
 chmod --verbose 700 "${HOME}/.ssh"
-stow --verbose -R -t ${HOME} flake8
-stow --verbose -R -t ${HOME} git
-stow --verbose -R -t ${HOME} gnupg
-stow --verbose -R -t ${HOME} htop
-stow --verbose -R -t ${HOME} neovim
-stow --verbose -R -t ${HOME} python
-stow --verbose -R -t ${HOME} shells
-stow --verbose -R -t ${HOME} ssh
-stow --verbose -R -t ${HOME} tmux
-stow --verbose -R -t ${HOME} virtualenvwrapper
-stow --verbose -R -t ${HOME} zsh
+stow --verbose -R -t ${HOME} --ignore=.venv flake8
+stow --verbose -R -t ${HOME} --ignore=.venv git
+stow --verbose -R -t ${HOME} --ignore=.venv gnupg
+stow --verbose -R -t ${HOME} --ignore=.venv htop
+stow --verbose -R -t ${HOME} --ignore=.venv neovim
+stow --verbose -R -t ${HOME} --ignore=.venv python
+stow --verbose -R -t ${HOME} --ignore=.venv shells
+stow --verbose -R -t ${HOME} --ignore=.venv ssh
+stow --verbose -R -t ${HOME} --ignore=.venv tmux
+stow --verbose -R -t ${HOME} --ignore=.venv virtualenvwrapper
+stow --verbose -R -t ${HOME} --ignore=.venv zsh
 
 if [[ ${UID} -ne 0 ]]; then
 	mkdir --verbose -p "${HOME}/.config/cmus"
@@ -22,14 +22,14 @@ if [[ ${UID} -ne 0 ]]; then
 	mkdir --verbose -p "${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
 	mkdir --verbose -p "${HOME}/.local/bin"
 	mkdir --verbose -p "${HOME}/.unison"
-	stow --verbose -R -t ${HOME} appearance
-	stow --verbose -R -t ${HOME} cmus
-	stow --verbose -R -t ${HOME} fontconfig
-	stow --verbose -R -t ${HOME} i3
-	stow --verbose -R -t ${HOME} pacman
-	stow --verbose -R -t ${HOME} pacaur
-	stow --verbose -R -t ${HOME} profile-cleaner
-	stow --verbose -R -t ${HOME} termite
-	stow --verbose -R -t ${HOME} unison
-	stow --verbose -R -t ${HOME} x.org
+	stow --verbose -R -t ${HOME} --ignore=.venv appearance
+	stow --verbose -R -t ${HOME} --ignore=.venv cmus
+	stow --verbose -R -t ${HOME} --ignore=.venv fontconfig
+	stow --verbose -R -t ${HOME} --ignore=.venv i3
+	stow --verbose -R -t ${HOME} --ignore=.venv pacman
+	stow --verbose -R -t ${HOME} --ignore=.venv pacaur
+	stow --verbose -R -t ${HOME} --ignore=.venv profile-cleaner
+	stow --verbose -R -t ${HOME} --ignore=.venv termite
+	stow --verbose -R -t ${HOME} --ignore=.venv unison
+	stow --verbose -R -t ${HOME} --ignore=.venv x.org
 fi

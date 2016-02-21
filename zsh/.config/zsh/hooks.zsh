1b55c49b6ca5b00e025dd924279d455edf178517
Raphael Scholer <rascholer@gmail.com>
2016-02-21 06:43:23 +0100

    zsh - Show available branches when entering a git repository
    Output is only shown when more than one branch exists.

--- zsh/.config/zsh/hooks.zsh
+++ zsh/.config/zsh/hooks.zsh
@@ -1,4 +1,5 @@
 autoload -Uz add-zsh-hook
 
+[[ -n  ${functions[chpwd-git]} ]] && add-zsh-hook chpwd chpwd-git
 [[ -n  ${functions[chpwd-venv]} ]] && add-zsh-hook chpwd chpwd-venv
 [[ -n  ${functions[precmd-rehash]} ]] && add-zsh-hook precmd precmd-rehash

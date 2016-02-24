a6c0332cbe04a8c14a54b47e2805704af95a8f8a
Raphael Scholer <rascholer@gmail.com>
2016-02-25 00:03:20 +0100

    stow_all.sh - Improve script
    - Use long arguments
    - Reduce calls of stow to max. 2
    - Decrease verbosity

--- stow_all.sh
+++ stow_all.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
-mkdir --verbose -p "${HOME}/.config/gnupg"
-mkdir --verbose -p "${HOME}/.local/share/virtualenvs"
-mkdir --verbose -p "${HOME}/.ssh"
-chmod --verbose 700 "${HOME}/.ssh"
-stow --verbose -R -t ${HOME} --ignore=.venv flake8
-stow --verbose -R -t ${HOME} --ignore=.venv git
-stow --verbose -R -t ${HOME} --ignore=.venv gnupg
-stow --verbose -R -t ${HOME} --ignore=.venv htop
-stow --verbose -R -t ${HOME} --ignore=.venv neovim
-stow --verbose -R -t ${HOME} --ignore=.venv python
-stow --verbose -R -t ${HOME} --ignore=.venv shells
-stow --verbose -R -t ${HOME} --ignore=.venv ssh
-stow --verbose -R -t ${HOME} --ignore=.venv tmux
-stow --verbose -R -t ${HOME} --ignore=.venv virtualenvwrapper
-stow --verbose -R -t ${HOME} --ignore=.venv zsh
+mkdir --verbose --parents \
+	"${HOME}/.config/gnupg" \
+	"${HOME}/.local/share/virtualenvs" \
+	"${HOME}/.ssh"
+chmod --changes 700 "${HOME}/.ssh"
+stow --restow --target "${HOME}" --ignore=.venv \
+	flake8 \
+	git \
+	gnupg \
+	htop \
+	neovim \
+	python \
+	shells \
+	ssh \
+	tmux \
+	virtualenvwrapper \
+	zsh
 
 if [[ ${UID} -ne 0 ]]; then
-	mkdir --verbose -p "${HOME}/.config/cmus"
-	mkdir --verbose -p "${HOME}/.config/gtk-2.0"
-	mkdir --verbose -p "${HOME}/.config/gtk-3.0"
-	mkdir --verbose -p "${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml"
-	mkdir --verbose -p "${HOME}/.local/bin"
-	mkdir --verbose -p "${HOME}/.unison"
-	stow --verbose -R -t ${HOME} --ignore=.venv appearance
-	stow --verbose -R -t ${HOME} --ignore=.venv cmus
-	stow --verbose -R -t ${HOME} --ignore=.venv fontconfig
-	stow --verbose -R -t ${HOME} --ignore=.venv i3
-	stow --verbose -R -t ${HOME} --ignore=.venv pacman
-	stow --verbose -R -t ${HOME} --ignore=.venv pacaur
-	stow --verbose -R -t ${HOME} --ignore=.venv profile-cleaner
-	stow --verbose -R -t ${HOME} --ignore=.venv rofi
-	stow --verbose -R -t ${HOME} --ignore=.venv snes9x
-	stow --verbose -R -t ${HOME} --ignore=.venv termite
-	stow --verbose -R -t ${HOME} --ignore=.venv unison
-	stow --verbose -R -t ${HOME} --ignore=.venv x.org
+	mkdir --verbose --parents \
+		"${HOME}/.config/cmus" \
+		"${HOME}/.config/gtk-2.0" \
+		"${HOME}/.config/gtk-3.0" \
+		"${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml" \
+		"${HOME}/.local/bin" \
+		"${HOME}/.unison"
+	stow --restow --target "${HOME}" --ignore=.venv \
+		appearance \
+		cmus \
+		fontconfig \
+		i3 \
+		pacman \
+		pacaur \
+		profile-cleaner \
+		rofi \
+		snes9x \
+		termite \
+		unison \
+		x.org
 fi

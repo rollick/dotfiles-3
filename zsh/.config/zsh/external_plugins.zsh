5a662ba7f87ac33a6d82f9c758bd1776f9e00884
Raphael Scholer <rascholer@gmail.com>
2016-03-23 07:26:32 +0100

    zsh - Don't wrap antigen inside a function

--- zsh/.config/zsh/external_plugins.zsh
+++ zsh/.config/zsh/external_plugins.zsh
@@ -1,27 +1,27 @@
-(){
-	local LIB="${XDG_DATA_HOME:-${HOME}/.local/share}/../lib/"
-	export ADOTDIR="${XDG_DATA_HOME:-${HOME}/.local/share}/antigen"
+LIB="${XDG_DATA_HOME:-${HOME}/.local/share}/../lib/"
+export ADOTDIR="${XDG_DATA_HOME:-${HOME}/.local/share}/antigen"
 
-	if [[ ! -d "${LIB}/antigen" ]]; then
-		mkdir -p "${LIB}/antigen"
+if [[ ! -d "${LIB}/antigen" ]]; then
+	mkdir -p "${LIB}/antigen"
 
-		git clone https://github.com/zsh-users/antigen.git "${LIB}/antigen"
-	fi
+	git clone https://github.com/zsh-users/antigen.git "${LIB}/antigen"
+fi
 
-	if [[ -f "${LIB}/antigen/antigen.zsh" ]]; then
-		source "${LIB}/antigen/antigen.zsh"
+if [[ -f "${LIB}/antigen/antigen.zsh" ]]; then
+	source "${LIB}/antigen/antigen.zsh"
 
-		[[ -n ${commands[git-flow]} ]] && antigen bundle petervanderdoes/git-flow-completion
-		antigen bundle hlissner/zsh-autopair
-		antigen bundle zsh-users/zsh-completions src
-		antigen bundle zsh-users/zsh-syntax-highlighting
+	[[ -n ${commands[git-flow]} ]] && antigen bundle petervanderdoes/git-flow-completion
+	antigen bundle hlissner/zsh-autopair
+	antigen bundle zsh-users/zsh-completions src
+	antigen bundle zsh-users/zsh-syntax-highlighting
 
-		antigen apply
+	antigen apply
 
-		=rm "${ZDOTDIR}/.zcompdump"
-		compinit -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/zcompdump"
-	fi
-}
+	=rm "${ZDOTDIR}/.zcompdump"
+	compinit -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/zcompdump"
+fi
+
+unset LIB
 
 # Include external additions provided by packages
 source "/usr/share/doc/pkgfile/command-not-found.zsh" &>|/dev/null

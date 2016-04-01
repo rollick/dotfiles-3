9639cdc0a22eb41e8ba2c57b9377e588ada0f1c3
Raphael Scholer <rascholer@gmail.com>
2016-04-01 07:02:49 +0200

    zsh - Add alias for udiskie-mount

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -82,6 +82,7 @@ alias mkdir="${aliases[mkdir]:-mkdir} --parents"
 [[ -n ${commands[rsync]} ]] && alias rsync="${aliases[rsync]:-rsync} --compress"
 [[ -n ${commands[sxiv]} ]] && alias sxiv="${aliases[sxiv]:-sxiv} -ar"
 alias tail="${aliases[tail]:-tail} -n \$(( \${LINES} - 2 * \$(print \${PROMPT} |wc -l) ))"
+alias udiskie-mount="${aliases[udiskie-mount]:-udiskie-mount} --recursive"
 alias zcalc="${aliases[zcalc]:-zcalc} -f"
 
 # Define new commands

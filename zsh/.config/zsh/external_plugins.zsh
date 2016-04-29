5579c3cc1f35da05e9b81713b6c045518fe2ce90
Raphael Scholer <rascholer@gmail.com>
2016-04-29 03:04:40 +0200

    zsh - Delete temp zplug install directory on exit

--- zsh/.config/zsh/external_plugins.zsh
+++ zsh/.config/zsh/external_plugins.zsh
@@ -1,18 +1,36 @@
+autoload -Uz add-zsh-hook
+
+function zplug_install_cleanup() {
+	if [[ -d "${ZPLUG_INSTALL_TMPDIR}" ]]; then
+		rm -rf "/${ZPLUG_INSTALL_TMPDIR}"
+	fi
+}
+
+
 export ZPLUG_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zplug"
 
 if [[ ! -d "${ZPLUG_HOME}" ]]; then
-	tmpdir="$(mktemp --directory)"
+	if [[ -d "${XDG_RUNTIME_DIR}" ]]; then
+		tmpdir="${XDG_RUNTIME_DIR}"
+	elif [[ -d "/run/user/${EUID}" ]]; then
+		tmpdir="/run/user/${EUID}"
+	else
+		tmpdir="/tmp"
+	fi
 
-	git clone --depth 1 "http://github.com/b4b4r07/zplug.git" "${tmpdir}"
+	export ZPLUG_INSTALL_TMPDIR="$(mktemp --directory --tmpdir=${tmpdir} zplug.XXXXXXXXXX)"
+	unset tmpdir
 
-	if [[ -f "${tmpdir}/zplug" ]]; then
+	git clone --depth 1 "http://github.com/b4b4r07/zplug.git" "${ZPLUG_INSTALL_TMPDIR}"
+
+	if [[ -f "${ZPLUG_INSTALL_TMPDIR}/zplug" ]]; then
 		mkdir -p "${ZPLUG_HOME}"
-		source "${tmpdir}/zplug"
+		source "${ZPLUG_INSTALL_TMPDIR}/zplug"
 
 		zplug update --self
 	fi
 
-	unset tmpdir
+	add-zsh-hook zshexit zplug_install_cleanup
 fi
 
 if [[ -d "${ZPLUG_HOME}" ]]; then

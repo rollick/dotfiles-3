de9d63a24fbf5590382aee69f67d368fbb90d252
Raphael Scholer <rascholer@gmail.com>
2016-07-12 23:57:40 +0200

    Revert "zsh - Remove unneccessary code"
    This reverts commit e7937d1fe2983cd350bcd3c1680cd75a2f6844d3.

--- zsh/.config/zsh/keybindings.zsh
+++ zsh/.config/zsh/keybindings.zsh
@@ -56,3 +56,18 @@ bindkey	'^Xe'					expand-word
 bindkey	'^Xk'					insert-kept-result
 bindkey	'^Xs'					toggle-sudo
 bindkey -M menuselect	'i'		accept-and-menu-complete
+
+# Make sure the terminal is in application mode, when zle is active.
+# Only then are the values from $terminfo valid.
+if [[ -n ${terminfo[smkx]} && ${+terminfo[rmkx]} ]]; then
+	function zle-line-init() {
+		print -n "${terminfo[smkx]}"
+	}
+
+	function zle-line-finish() {
+		print -n "${terminfo[rmkx]}"
+	}
+
+	zle -N zle-line-init
+	zle -N zle-line-finish
+fi

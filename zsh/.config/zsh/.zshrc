67acca9a50b7c1cc39c3120c17bcd25ede480ea8
Raphael Scholer <rascholer@gmail.com>
2017-09-23 17:10:21 +0200

    zsh/zshrc - Move check for EUID into the corresponding file

--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -30,7 +30,7 @@ source "${ZDOTDIR}/completions.zsh"
 source "${ZDOTDIR}/keybindings.zsh"
 source "${ZDOTDIR}/hooks.zsh"
 source "${ZDOTDIR}/aliases.zsh"
-[[ ${EUID} -ne 0 ]] && source "${ZDOTDIR}/external_plugins.zsh"
+source "${ZDOTDIR}/external_plugins.zsh"
 
 # Set prompt
 if [[ -n ${functions[prompt_simple_setup]} ]]; then
--- zsh/.config/zsh/external_plugins.zsh
+++ zsh/.config/zsh/external_plugins.zsh
@@ -1,17 +1,21 @@
 #!/bin/zsh
-# Include external additions provided by packages
-() {
-	local GIT_ROOT="${XDG_DOCUMENTS_DIR:-${HOME}/Documents}/Code/Remote"
+# Include external additions provided by packages, but only
+# if the current user is not root.
 
-	source "${GIT_ROOT}/zsh-autopair/autopair.zsh"
-	source "${GIT_ROOT}/git-flow-completion/git-flow-completion.plugin.zsh"
-	source "${GIT_ROOT}/zsh-completions/zsh-completions.plugin.zsh"
-	source "${GIT_ROOT}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+if [[ ${EUID} -ne 0 ]]; then
+	() {
+		local GIT_ROOT="${XDG_DOCUMENTS_DIR:-${HOME}/Documents}/Code/Remote"
 
-	source "/usr/bin/virtualenvwrapper_lazy.sh"
-	source "/etc/zsh_command_not_found"
-	source "/usr/share/doc/pkgfile/command-not-found.zsh"
-} &>|/dev/null
+		source "${GIT_ROOT}/zsh-autopair/autopair.zsh"
+		source "${GIT_ROOT}/git-flow-completion/git-flow-completion.plugin.zsh"
+		source "${GIT_ROOT}/zsh-completions/zsh-completions.plugin.zsh"
+		source "${GIT_ROOT}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
-# Plugin specific settings
-export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+		source "/usr/bin/virtualenvwrapper_lazy.sh"
+		source "/etc/zsh_command_not_found"
+		source "/usr/share/doc/pkgfile/command-not-found.zsh"
+	} &>|/dev/null
+
+	# Plugin specific settings
+	export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+fi

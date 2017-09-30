c6ae238d3e13eb823fabf9ae104de5926c6bcd90
Raphael Scholer <rascholer@gmail.com>
2017-09-30 11:08:10 +0200

    python - Use user-installed python documentation, if available

--- python/.config/profile.d/10-python.sh
+++ python/.config/profile.d/10-python.sh
@@ -4,3 +4,10 @@ export PYTHONSTARTUP="${XDG_CONFIG_HOME:-${HOME}/.config}/python.py"
 
 # Set path for virtualenvwrapper
 export WORKON_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/virtualenvs"
+
+# Set path to Python documentation
+if [[ -z ${PYTHONDOCS} ]]; then
+	if [[ -d "${XDG_PUBLICSHARE_DIR:-${HOME}/Public/python-docs}" ]]; then
+		export PYTHONDOCS="${XDG_PUBLICSHARE_DIR:-${HOME}/Public/python-docs}"
+	fi
+fi
